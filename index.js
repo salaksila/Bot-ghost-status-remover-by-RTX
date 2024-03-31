ee77d448b8e5c017b66669d760962699be7110c4
salaksila <165607322+salaksila@users.noreply.github.com>
2024-04-01 01:54:15 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Partner yapıyor","Partner için partner yazın"];
 
 
 let currentIndex = 0;
